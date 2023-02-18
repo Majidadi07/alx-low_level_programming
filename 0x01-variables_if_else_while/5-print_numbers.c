d93db27103a9647d2561fb1e57d3cf5213af8ed8
Majidadi07 <nuruhayatu17@gmail.com>
2023-02-17 19:10:01 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Starting function
+ *
+ * Return: provie 0 error for success run
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
