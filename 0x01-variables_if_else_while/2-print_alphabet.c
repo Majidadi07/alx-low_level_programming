da5a45ace4b2a48ee406790c2a5410f9677d5f1a
Majidadi07 <nuruhayatu17@gmail.com>
2023-02-17 18:19:22 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prinmary entry or starting point
+ *
+ * Return: give 0 for success
+ *
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+		putchar('\n');
+	}
+	return (0);
+}
