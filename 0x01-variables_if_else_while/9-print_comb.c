2616f81b0db06e6ba7ad1636b9070e7905bf2c7f
Majidadi07 <nuruhayatu17@gmail.com>
2023-02-18 05:37:41 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - starting point of running the program
+ *
+ * Return: provide 0 error for a successful run
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
