218f5cebe7e9e6426087fd55d44b7dc386d593f6
Majidadi07 <nuruhayatu17@gmail.com>
2023-02-22 16:32:48 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include "main.h"
 /**
  * main - main program start
  *
@@ -11,8 +12,23 @@ int _putchar(char c);
 
 int main(void)
 {
-	char c='c';
+	char c = '-';
+	char d = 'p';
+	char e = 'u';
+	char f = 't';
+	char g = 'c';
+	char h = 'h';
+	char i = 'a';
+	char j = 'r';
+
 	_putchar(c);
+	_putchar(d);
+	_putchar(e);
+	_putchar(f);
+	_putchar(g);
+	_putchar(h);
+	_putchar(i);
+	_putchar(j);
 
 	return (0);
 }
