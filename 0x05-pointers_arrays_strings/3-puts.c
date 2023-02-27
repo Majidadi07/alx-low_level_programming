eac7389e161dd4189f14a1af5534fd4c0677e29c
Majidadi07 <nuruhayatu17@gmail.com>
2023-02-27 15:07:25 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function to print the string entered
+ * @str: a char variable as parameter
+ * Return: a void function, no return
+ */
+
+void _puts(char *str)
+{
+	int a;
+
+	a = 0;
+
+	while (str[a] != '\0')
+		_putchar(str[a]);
+		a++;
+	
+	_putchar('\n');
+}
