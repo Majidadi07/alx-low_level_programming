5f50c94f91b7405bb7ff1488ee37d1685e312224
Majidadi07 <nuruhayatu17@gmail.com>
2023-02-27 17:49:41 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copy the string pointed to by src
+ * 
+ * @dest: last point
+ * @src: starting point or source
+ *
+ * Return: a character
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *a = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (a);
+}
