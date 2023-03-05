9a2ff891b6c92dfac0dfa0c91b6a64bf0f717e7c
Majidadi07 <nuruhayatu17@gmail.com>
2023-03-05 03:48:53 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ * Return: return 0 for a run without error
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz")
+		}
+		else
+		{
+			printf("%d", a);
+		}
+		if (a != 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
