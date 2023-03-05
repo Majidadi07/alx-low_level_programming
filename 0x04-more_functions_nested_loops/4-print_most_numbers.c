a73d839161b34bc644a56bb4d2c8fcbad985e07f
Majidadi07 <nuruhayatu17@gmail.com>
2023-03-05 01:11:02 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,11 @@ void print_most_numbers(void)
 	int a;
 
 	for (a = '0'; a <= '9'; a++)
-		if (a == '2' || a == '4')
-		continue;
-		_putchar(a);
+	{
+		if ((a == '2') || (a == '4'))
+			continue;
+		else
+			_putchar(a);
+	}
 	_putchar('\n');
 }
