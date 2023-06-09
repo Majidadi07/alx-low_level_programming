94dae04560abf9ae1dbb212aee8bc294d9499b6b
Majidadi07 <nuruhayatu17@gmail.com>
2023-06-08 17:24:36 -0700

    Betty checks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a given string followed by a new line 
+ * _puts_recursion - function that prints a given string followed by a new line
  * @s: Is the given string
  *
  * Return: success if there is no error
@@ -9,7 +9,7 @@
 
 void _puts_recursion(char *s)
 {
-	if(*s == 0)
+	if (*s == 0)
 	{
 		_putchar('\n');
 		return;
