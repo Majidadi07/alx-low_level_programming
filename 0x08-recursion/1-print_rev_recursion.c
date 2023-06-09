6c9c7676dccd0e89a310513e8c521cc8830e0b70
Majidadi07 <nuruhayatu17@gmail.com>
2023-06-08 17:02:01 -0700

    Commitinh 2 work

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print the input s in reverse
+ * @s: the input string
+ *
+ * Return: give success when no error
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if(*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+0x08-recursion
+
+Learning how recursion works
